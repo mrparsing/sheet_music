eef942bc03351aa1a8373e44206f742747096efe
mrparsing <fralindi5@gmail.com>
2025-01-05 22:02:31 +0100

    update

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -354,7 +354,9 @@ function inserisci_elemento_lista(numero, anno, tipologia) {
         } else if (numero === "Solennità di tutti i Santi") {
             li.innerHTML = `<a href="celebrazioni.html?numero=${numero}&anno=${anno}&festivita=ordinario">${numero} - anno: ${anno}</a>`;
         } else {
-            li.innerHTML = `<a href="celebrazioni.html?numero=${numero}&anno=${anno}&festivita=ordinario">${convertiInRomano(numero)} domenica tempo ordinario - anno: ${anno}</a>`;
+            //li.innerHTML = `<a href="celebrazioni.html?numero=${numero}&anno=${anno}&festivita=ordinario">${convertiInRomano(numero)} domenica tempo ordinario - anno: ${anno}</a>`;
+            li.innerHTML = `<a href="celebrazione/${numero}-domenica-tempo-ordinario-anno-${anno}">${convertiInRomano(numero)} domenica tempo ordinario - anno: ${anno}</a>`;
+
         }
     } else if (tipologia === "avvento" && !numero.includes("Novena Immacolata")) {
         li.innerHTML = `<a href="celebrazioni.html?numero=${numero}&anno=${anno}&festivita=avvento">${convertiInRomano(numero)} domenica tempo d'avvento - anno: ${anno}</a>`;
