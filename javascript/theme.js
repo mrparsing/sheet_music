8b02d343b71584678d19afac8952914a3f71c03b
mrparsing <fralindi5@gmail.com>
2025-09-12 11:02:12 +0200

    .

--- javascript/theme.js
+++
@@ -1,7 +0,0 @@
-window.onload = function () {
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme === 'dark') {
-        console.log("ciao", savedTheme);
-        document.body.classList.add('dark-theme');
-    }
-}
